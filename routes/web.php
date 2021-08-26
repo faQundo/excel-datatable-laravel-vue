aa1ff4160e50108f48e58ae9da392c740c2703f5
faqundo <facundo.ferro@werden.com.ar>
2021-08-26 10:31:38 -0300

    fix route where any view aplicattion web

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('application');
-});
-
-/* Route::get('/{any}', function () {
-    return view('application')->where('any', '.*');
 }); */
+
+Route::get('/{any}', function () {
+    return view('application');
+})->where(['any' => '.*']);
